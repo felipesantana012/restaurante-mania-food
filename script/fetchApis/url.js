5b18c93a8d0cc7276d7020e5da8946dcb90ce1cc
Felipe Santana <felipe012santana012@gmail.com>
2024-08-06 02:22:17 -0300

    Correção url

--- script/fetchApis/url.js
+++ script/fetchApis/url.js
@@ -1,6 +1,6 @@
-export const url = "https://back-end-mania-food.vercel.app";
+// export const url = "https://back-end-mania-food.vercel.app";
 
-// export const url =
-//   "https://my-json-server.typicode.com/felipesantana012/api-json-mania-food/";
+export const url =
+  "https://my-json-server.typicode.com/felipesantana012/api-json-mania-food/";
 
 // export const url = "http://localhost:5001";
