060243431b85fc879dd7978181a68475f114f11c
Felipe Santana <felipe012santana012@gmail.com>
2024-08-06 19:39:59 -0300

    back Url do railway

--- script/fetchApis/url.js
+++ script/fetchApis/url.js
@@ -1,8 +1,10 @@
 // export const url = "https://back-end-mania-food.vercel.app";
 
-export const url = "https://backend-mania-food.onrender.com";
+// export const url = "https://backend-mania-food.onrender.com";
 
 // export const url =
 //   "https://my-json-server.typicode.com/felipesantana012/api-json-mania-food/";
 
 // export const url = "http://localhost:5001";
+
+export const url = "https://backend-mania-food-production.up.railway.app";
