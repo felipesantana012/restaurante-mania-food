b433d20ce3d4c5676cf845e328e5f63d6d3cb51a
Felipe Santana <felipe012santana012@gmail.com>
2024-08-06 16:18:24 -0300

    Correção de url back end para onrender

--- script/fetchApis/url.js
+++ script/fetchApis/url.js
@@ -1,4 +1,6 @@
-export const url = "https://back-end-mania-food.vercel.app";
+// export const url = "https://back-end-mania-food.vercel.app";
+
+export const url = "https://backend-mania-food.onrender.com";
 
 // export const url =
 //   "https://my-json-server.typicode.com/felipesantana012/api-json-mania-food/";
